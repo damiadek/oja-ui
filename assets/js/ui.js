3d8b2d7a4fe7fb1a18cd6db460f7e090a46c9680
damiadek <dami_dare2009@yahoo.com>
2017-05-17 12:52:02 +0100

    Signup stuff

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-    // $(".form-div").fadeIn(2000);
+	// init tooltip
+	$(".tip").tooltip();
+
+	// add spin on hover
+	$(".spin").hover(function(){
+		$(this).find(".spin-child").addClass("fa-spin");
+	}, function(){
+		$(this).find(".spin-child").removeClass("fa-spin");
+	});
+
+	// toggle dropdown arrow
+	$(".dropdown-toggle").click(function(){
+		$(this).find("b").toggleClass("fa-angle-down");
+		$(this).find("b").toggleClass("fa-angle-up");
+	});
+
+	// swap login and signup divs
+	$(".swap_button").click(function(e){
+		addSpinner($(this));
+		e.preventDefault();
+		var $target = $( "." + $(this).attr("target"));
+		var $parent = $(this).closest("fieldset");
+		$parent.slideUp(400, function(){
+			$parent.removeClass("active");
+			$target.addClass("active").slideDown();
+		});
+		removeSpinner
+	});
 });
